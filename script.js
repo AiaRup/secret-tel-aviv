e733df63e26537b13cf9447da3a0b90d00d1134a
AiaRup <aiale88@gmail.com>
2018-03-21 11:32:39 +0200

    finished partner exercises

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function(){
+    $('html, body').animate({
+      scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
+});
